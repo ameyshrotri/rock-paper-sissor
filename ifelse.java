50407ffb9ad0f0e2e0bfb33f8d3fd16a62ef89e4
ameyshrotri <70018831+ameyshrotri@users.noreply.github.com>
2021-05-10 16:35:55 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+public class ifelse {
+    public static void main(String[] args) {
+    /* int a , b , c;
+     float avg;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a");
+        a = sc.nextInt();
+        System.out.println("enter b");
+        b = sc.nextInt();
+        System.out.println("enter c");
+        c = sc.nextInt();
+
+        avg = (a+b+c)/3.0f;
+        System.out.println("avg"+avg);
+
+      if (avg>40 && a>33 && b>33 && c>33){
+          System.out.println("congrats ur pass");
+      }
+      else {
+          System.out.println("sry ur fail");
+      }*/
+
+
+      /*  Scanner sc = new Scanner(System.in);
+        System.out.println("enter day");
+        int day = sc.nextInt();
+
+      switch (day){
+          case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+           case 3 -> System.out.println("Wednesday");
+           case 4 -> System.out.println("Thursday");
+           case 5 -> System.out.println("Friday");
+          case 6 -> System.out.println("Saturday");
+           case 7 -> System.out.println("Sunday");
+        }*/
+        Random r = new Random();
+        int a = r.nextInt();
+        System.out.println(a);
+
+    }
+}
+
